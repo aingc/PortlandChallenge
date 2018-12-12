2bb36b5f34e01320a3642e9fceaa44773e495e9e
Corey <corey.aing@tiger-sheep.com>
2018-12-11 18:29:33 -0800

    Deduplication is done. Need to implement logging next

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 var fs = require('fs');
-var obj = JSON.parse(fs.readFileSync('leads.json', 'utf8'));
-console.log(obj);
+var records = JSON.parse(fs.readFileSync('leads.json', 'utf8'));
+var deduplicatedRecords = [];
+
+const checkCollision = (recordElement) => {
+	if (deduplicatedRecords.length === 0)
+		return false;
+	else {
+		for (let i = 0; i < deduplicatedRecords.length; i++) {
+			if (recordElement._id === deduplicatedRecords[i]._id || recordElement.email === deduplicatedRecords[i].email) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+const deduplicate = (recordObjects) => {
+	for (let i = recordObjects.length - 1; i >= 0; i--) {
+		if (!checkCollision(recordObjects[i])) {
+			//add to deduplicatedRecords
+			deduplicatedRecords.push(recordObjects[i]);
+		}
+	}
+	return deduplicatedRecords;
+}
+
+console.log(deduplicate(records.leads).reverse());
